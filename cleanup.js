df272eb77e47fddbbc0c5a75ddbabaab1059f5ba
Anatoly Laskaris <github_me@nahsi.dev>
2023-05-08 14:49:00 +0300

    Fix

---
+++ cleanup.js
@@ -0,0 +1,19 @@
+const core = require("@actions/core");
+const fs = require("fs").promises;
+
+async function cleanup() {
+  try {
+    const tmpDir = core.getState("tmpDir");
+
+    if (tmpDir) {
+      core.info(`Cleaning up temporary IPFS directory: ${tmpDir}`);
+      await fs.rmdir(tmpDir, { recursive: true });
+    } else {
+      core.warning("Temporary IPFS directory not found");
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+cleanup();
